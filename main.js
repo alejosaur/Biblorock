58aece63f71cb80b355bbd2a918fcc2d3bc9468d
alejosaur <asantamariap@unal.edu.co>
2017-12-13 22:05:04 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-//algo
+var config = {
+    apiKey: "AIzaSyCZRZjcpCLuydPEw4aGP_vdpIc7Mlnwn70",
+    authDomain: "biblorock.firebaseapp.com",
+    databaseURL: "https://biblorock.firebaseio.com",
+    projectId: "biblorock",
+    storageBucket: "biblorock.appspot.com",
+    messagingSenderId: "821897959326"
+};
+firebase.initializeApp(config);
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+$("#login").click(function(){
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then(function(result){
+            console.log(result.user);
+            $("#login").hide();
+            guardarDatos(result.user);
+        });
+});
+
+function guardarDatos(user){
+    var usuario = {
+        uid:user.uid,
+        nombre:user.displayName,
+        email:user.email,
+        foto:user.photoURL,
+        certified:false
+    }
+    firebase.database().ref("usuarios/"+user.uid).set(usuario);
+}
